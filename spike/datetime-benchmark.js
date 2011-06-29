8ceffa342e59debeaa65e8fee2cfbda263f1dd18
Mathias Buus <mathiasbuus@gmail.com>
2011-06-29 22:31:13 +0200

    added a fast compiledate spike and updated benchmark

--- spike/datetime-benchmark.js
+++ spike/datetime-benchmark.js
@@ -1,9 +1,10 @@
 var verbose = require('./datepattern');
 var regex = require('./compiledate');
+var fastcompile = require('./compiledatefast');
 
 var subject = new Date('august 22 2011 10:30:00');
 
-var amount = 10000;
+var amount = 100000;
 var count = 0;
 
 var funcs = [];
@@ -40,6 +41,26 @@ for(var i=0 ; i < amount; i++) {
 }
 var time = Date.now() - then;
 var rb = Math.round(count/time);
-console.log(rb + ' matches/ms Regex ' + time + ' ms');
+console.log(rb + ' matches/ms Compile ' + time + ' ms');
 
-console.log(Math.round((vb*100)/rb) + '% Verbose vs. Regex');
+/*******************************************************/
+
+var count = 0;
+var funcs = [];
+
+for(var j in fastcompile.tests) {
+	funcs.push(fastcompile.match(fastcompile.tests[j]));		
+}
+
+var then = Date.now();
+for(var i=0 ; i < amount; i++) {
+	for(var j in funcs) {
+		count++;
+		funcs[j](subject);
+	}
+}
+var time = Date.now() - then;
+var rb = Math.round(count/time);
+console.log(rb + ' matches/ms Compile-Fast ' + time + ' ms');
+
+console.log(Math.round((vb*100)/rb) + '% Verbose vs Compile-Fast');
